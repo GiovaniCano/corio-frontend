5e0eb0426871a11c2b9e26a28973fdc3a0fe1f6c
GiovaniCano <g.giovani.cano@outlook.com>
2022-12-13 11:52:52 -0600

    ready to build and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: '//127.0.0.1:8000/api/',
+  API_URL: '//corio.fly.dev/api/',
+  // API_URL: '//127.0.0.1:8000/api/',
+  APP_NAME: 'Corio'
 };
